bf1a65abef965149332fc771345c7939997929c3
AndreyShi <expoder7@gmail.com>
2024-06-10 22:08:15 +0300

    new way read fifo DMP

--- MPU6050_example_1.cpp
+++ MPU6050_example_1.cpp
@@ -142,7 +142,29 @@ int main(int argc, char **argv) {
     printf("Int: %d  ",mpu.getMotionStatus());
 
     // read a packet from FIFO
-    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) 
+      mpuIntStatus = mpu.getIntStatus();
+
+  // get current FIFO count
+  fifoCount = mpu.getFIFOCount();
+
+  // check for overflow (this should never happen unless our code is too inefficient)
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+    // reset so we can continue cleanly
+    mpu.resetFIFO();
+    Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+  } else if (mpuIntStatus & 0x02) {
+    // wait for correct available data length, should be a VERY short wait
+    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+    // read a packet from FIFO
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+    // track FIFO count here in case there is > 1 packet available
+    // (this lets us immediately read more without waiting for an interrupt)
+    fifoCount -= packetSize;
+    //if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) 
     //mpu.getFIFOBytes(fifoBuffer, packetSize);
     { // Get the Latest packet 
             mpu.dmpGetQuaternion(&q, fifoBuffer);
@@ -162,6 +184,7 @@ int main(int argc, char **argv) {
     }
     else
         { printf("mpu.dmpGetCurrentFIFOPacket false\n");}
+    }
   }
   // use the code below to change accel/gyro offset values
   /*
