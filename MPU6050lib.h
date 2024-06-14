f472d263d7a8e4485639729475926b0593f9d25d
AndreyShi <expoder7@gmail.com>
2024-06-15 00:19:13 +0300

    for Python compability

--- MPU6050lib.h
+++ MPU6050lib.h
@@ -1,13 +1,21 @@
-float get_yarn(void);
-float get_pitch(void);
-float get_roll(void);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-float get_AccX(void);
-float get_AccY(void);
-float get_AccZ(void);
+    float get_yarn(void);
+    float get_pitch(void);
+    float get_roll(void);
 
-float get_GyroX(void);
-float get_GyroY(void);
-float get_GyroZ(void);
+    float get_AccX(void);
+    float get_AccY(void);
+    float get_AccZ(void);
 
-int init_MPU(void);
+    float get_GyroX(void);
+    float get_GyroY(void);
+    float get_GyroZ(void);
+
+    int init_MPU(void);
+
+#ifdef __cplusplus
+}
+#endif
