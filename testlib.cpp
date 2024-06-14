df1b7d1b0dba5b6ef74f919f495ce156cd478552
rasberryPi <andrey.shilyaev.89@mail.ru>
2024-06-14 14:10:02 +0300

    fix testlib

--- testlib.cpp
+++ testlib.cpp
@@ -1,4 +1,6 @@
+#include <bcm2835.h>
 #include <pthread.h>
+#include <cstdio>
 #include "MPU6050lib.h"
 
 
@@ -10,8 +12,9 @@ int main(int argc, char **argv) {
    void* thread_data = NULL;
    pthread_t thread;
  
-   pthread_create(&thread, NULL, threadFunc(), thread_data);
+   pthread_create(&thread, NULL, threadFunc, thread_data);
    while(1){
+        delay(1000);
         printf("main thread\n");
    }
    pthread_join(thread, NULL);
