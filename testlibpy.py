4f6cb5240a243b3aec189271b5442d2fcb1d9c04
AndreyShi <expoder7@gmail.com>
2024-07-05 11:00:26 +0300

    correct way to get float from C++ lib

--- testlibpy.py
+++ testlibpy.py
@@ -1,8 +1,18 @@
 from threading import Thread
 from time import sleep
 from ctypes import cdll
+from ctypes import *
 
 MPU = cdll.LoadLibrary('./libMPU6050.so')
+MPU.get_yarn.restype = c_float
+MPU.get_pitch.restype = c_float
+MPU.get_roll.restype = c_float
+MPU.get_AccX.restype = c_float
+MPU.get_AccY.restype = c_float
+MPU.get_AccZ.restype = c_float
+MPU.get_GyroX.restype = c_float
+MPU.get_GyroY.restype = c_float
+MPU.get_GyroZ.restype = c_float
 
 def func():
 	MPU.init_MPU()
