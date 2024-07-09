d05967f0cb70df2be451b694fd67d78fb773053e
AndreyShi <expoder7@gmail.com>
2024-07-09 20:35:32 +0300

    fix client

--- client.py
+++ client.py
@@ -8,10 +8,11 @@ bufferSize=1024
 UDPClient=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 UDPClient.sendto(bytesToSend,serverAddress)
 data,address=UDPClient.recvfrom(bufferSize)
-msg=data.decode('utf-8')
 if msgFromClient == 'give_MPU_bin_data':
  obj_float = struct.unpack_from('!fffffffff',data) 
  print(obj_float)
-print('Data from server: ',msg)
+else:
+ msg=data.decode('utf-8')
+ print('Data from server: ',msg)
 print('Server IP Addres: ',address[0])
 print('Server Port: ',address[1])
