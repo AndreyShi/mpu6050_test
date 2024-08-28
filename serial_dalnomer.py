a9e46e001e9e676e73a8e1aa295eccc3e0cc807e
raspberry <_>
2024-08-28 09:46:34 +0100

    uart from py ok

--- serial_dalnomer.py
+++ serial_dalnomer.py
@@ -1,10 +1,10 @@
 import serial
 
 ser = serial.Serial('/dev/ttyS0', 115200, timeout=1)
-ser.open()
+#ser.open()
 if ser.isOpen():
     print('Serial connection established!')
-    ser.write('<MAcont>')
+    ser.write(str.encode('<MAcont>'))
 else:
     print('Error: Failed to establish serial connection.')
 ser.close()
