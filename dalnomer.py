3f9a994e88485221f046d60d3088439aef8e8f78
a.shiliaev <andrey.shilyayev.89@mail.ru>
2024-08-29 13:36:46 +0300

    update

--- dalnomer.py
+++ dalnomer.py
@@ -2,13 +2,13 @@ import serial
 
 distance = 'measurment distance from device'
 def poll():
- ser = serial.Serial('/dev/ttyS0', 115200, timeout=2)
+ ser = serial.Serial('/dev/ttyS0', 115200, timeout=3)
  #ser.open()
  if ser.isOpen():
     print('Serial connection established!')
     ser.write(str.encode('<MAcont>'))
     while 1:
-        distance = ser.read()
+        distance = ser.read(10) #1200.00m
  else:
     print('Error: Failed to establish serial connection.')
  ser.close()
